fe450ca56fb4f18cd25496269670a641476590e9
Valentina <valenovalles@gmail.com>
2024-05-31 10:57:10 +0200

    act

--- bd/netflix.sql
+++ bd/netflix.sql
@@ -1,195 +1,171 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: netflix
+-- ------------------------------------------------------
+-- Server version	8.0.37
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+--
+-- Table structure for table `actors`
+--
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema netflix
--- -----------------------------------------------------
+DROP TABLE IF EXISTS `actors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actors` (
+  `idActor` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  PRIMARY KEY (`idActor`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Schema netflix
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `netflix` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `mydb` ;
+--
+-- Dumping data for table `actors`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`promos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`promos` (
-  `idtable1` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `year` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idtable1`))
-ENGINE = InnoDB;
+LOCK TABLES `actors` WRITE;
+/*!40000 ALTER TABLE `actors` DISABLE KEYS */;
+INSERT INTO `actors` VALUES (1,'Tom','Hanks','Estados Unidos','1956-07-09'),(2,'Roberto','Benigni','Italia','1952-10-27'),(3,'John','Travolta','Estados Unidos','1954-02-18');
+/*!40000 ALTER TABLE `actors` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `actors_has_movie`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`alumans`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`alumans` (
-  `idalumans` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `lastname` VARCHAR(45) NOT NULL,
-  `FkPromo` INT NOT NULL,
-  PRIMARY KEY (`idalumans`),
-  INDEX `fk_alumans_promos_idx` (`FkPromo` ASC) VISIBLE,
-  CONSTRAINT `fk_alumans_promos`
-    FOREIGN KEY (`FkPromo`)
-    REFERENCES `mydb`.`promos` (`idtable1`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`profes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`profes` (
-  `idprofes` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idprofes`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`profes_has_promos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`profes_has_promos` (
-  `fkProfes` INT NOT NULL,
-  `fkPromos` INT NOT NULL,
-  `IdPromoProfe` INT NOT NULL AUTO_INCREMENT,
-  INDEX `fk_profes_has_promos_promos1_idx` (`fkPromos` ASC) VISIBLE,
-  INDEX `fk_profes_has_promos_profes1_idx` (`fkProfes` ASC) VISIBLE,
-  PRIMARY KEY (`IdPromoProfe`),
-  CONSTRAINT `fk_profes_has_promos_profes1`
-    FOREIGN KEY (`fkProfes`)
-    REFERENCES `mydb`.`profes` (`idprofes`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_profes_has_promos_promos1`
-    FOREIGN KEY (`fkPromos`)
-    REFERENCES `mydb`.`promos` (`idtable1`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-USE `netflix` ;
-
--- -----------------------------------------------------
--- Table `netflix`.`actors`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `netflix`.`actors` (
-  `idActor` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `lastName` VARCHAR(45) NOT NULL,
-  `country` VARCHAR(45) NOT NULL,
-  `birthday` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`idActor`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `netflix`.`movie`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `netflix`.`movie` (
-  `idMovies` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL,
-  `genre` VARCHAR(45) NOT NULL,
-  `image` VARCHAR(1000) NOT NULL,
-  `category` VARCHAR(45) NOT NULL,
-  `year` SMALLINT NULL DEFAULT NULL,
-  PRIMARY KEY (`idMovies`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `netflix`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `netflix`.`user` (
-  `idUser` INT NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `plan_details` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idUser`),
-  UNIQUE INDEX `user` (`user` ASC) VISIBLE,
-  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `netflix`.`movie_has_user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `netflix`.`movie_has_user` (
-  `fkMovies` INT NOT NULL,
-  `fkUser` INT NOT NULL,
-  `idMovieUser` INT NOT NULL AUTO_INCREMENT,
-  INDEX `fk_movie_has_user_user1_idx` (`fkUser` ASC) VISIBLE,
-  INDEX `fk_movie_has_user_movie_idx` (`fkMovies` ASC) VISIBLE,
-  PRIMARY KEY (`idMovieUser`),
-  CONSTRAINT `fk_movie_has_user_movie`
-    FOREIGN KEY (`fkMovies`)
-    REFERENCES `netflix`.`movie` (`idMovies`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_movie_has_user_user1`
-    FOREIGN KEY (`fkUser`)
-    REFERENCES `netflix`.`user` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `netflix`.`actors_has_movie`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `netflix`.`actors_has_movie` (
-  `fkActors` INT NOT NULL,
-  `fkMovie` INT NOT NULL,
-  `idActorMovie` INT NOT NULL AUTO_INCREMENT,
-  INDEX `fk_actors_has_movie_movie1_idx` (`fkMovie` ASC) VISIBLE,
-  INDEX `fk_actors_has_movie_actors1_idx` (`fkActors` ASC) VISIBLE,
+DROP TABLE IF EXISTS `actors_has_movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actors_has_movie` (
+  `fkActors` int NOT NULL,
+  `fkMovie` int NOT NULL,
+  `idActorMovie` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`idActorMovie`),
-  CONSTRAINT `fk_actors_has_movie_actors1`
-    FOREIGN KEY (`fkActors`)
-    REFERENCES `netflix`.`actors` (`idActor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_actors_has_movie_movie1`
-    FOREIGN KEY (`fkMovie`)
-    REFERENCES `netflix`.`movie` (`idMovies`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_actors_has_movie_movie1_idx` (`fkMovie`),
+  KEY `fk_actors_has_movie_actors1_idx` (`fkActors`),
+  CONSTRAINT `fk_actors_has_movie_actors1` FOREIGN KEY (`fkActors`) REFERENCES `actors` (`idActor`),
+  CONSTRAINT `fk_actors_has_movie_movie1` FOREIGN KEY (`fkMovie`) REFERENCES `movie` (`idMovies`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SELECT * FROM movie_has_user;
-ALTER TABLE  movie_has_user ADD score INT;
+--
+-- Dumping data for table `actors_has_movie`
+--
 
-SELECT * FROM actors_has_movie;
+LOCK TABLES `actors_has_movie` WRITE;
+/*!40000 ALTER TABLE `actors_has_movie` DISABLE KEYS */;
+INSERT INTO `actors_has_movie` VALUES (3,1,1),(2,2,2),(1,3,3);
+/*!40000 ALTER TABLE `actors_has_movie` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `movie`
+--
 
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie` (
+  `idMovies` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `genre` varchar(45) NOT NULL,
+  `image` varchar(1000) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `year` smallint DEFAULT NULL,
+  PRIMARY KEY (`idMovies`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+INSERT INTO `movie` VALUES (1,' Pulp Fiction','Crimen','https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg','Top 10',1994),(2,' La vita è bella','Comedia','https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg','Top 10',1996),(3,' Forrest Gump','Comedia','https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg','Top 10',1994);
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie_has_user`
+--
+
+DROP TABLE IF EXISTS `movie_has_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie_has_user` (
+  `fkMovies` int NOT NULL,
+  `fkUser` int NOT NULL,
+  `idMovieUser` int NOT NULL AUTO_INCREMENT,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`idMovieUser`),
+  KEY `fk_movie_has_user_user1_idx` (`fkUser`),
+  KEY `fk_movie_has_user_movie_idx` (`fkMovies`),
+  CONSTRAINT `fk_movie_has_user_movie` FOREIGN KEY (`fkMovies`) REFERENCES `movie` (`idMovies`),
+  CONSTRAINT `fk_movie_has_user_user1` FOREIGN KEY (`fkUser`) REFERENCES `user` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie_has_user`
+--
+
+LOCK TABLES `movie_has_user` WRITE;
+/*!40000 ALTER TABLE `movie_has_user` DISABLE KEYS */;
+INSERT INTO `movie_has_user` VALUES (1,1,1,10),(2,1,2,8),(2,2,3,10);
+/*!40000 ALTER TABLE `movie_has_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `plan_details` varchar(45) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `user` (`user`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'laura_dev','laura','Laura','laura@gmail.com','Standard'),(2,'maria_dev','maria','Maria','maria@gmail.com','Standard'),(3,'ester_dev','ester','Ester','ester@gmail.com','Standard');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-31 10:54:34
