fe170be82f326d644842e0f49a90c0dca28e93a4
Valentina <valenovalles@gmail.com>
2024-06-07 11:11:21 +0200

    plantillas

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const mysql = require("mysql2/promise")
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.set('view engine', 'ejs');
 
 async function connectDB () {
   //los datos corresponden a la bd de tu ordenador
   const conex = await mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "Ellie.bellie1993",
+      password: "etsitoet87.",
       database: "netflix"
   });
   await conex.connect();
@@ -45,8 +46,22 @@ server.get('/movies', async (req, res)=>{
 
 });
 
+server.get('/movie/:idMovies', async (req, res) => { 
+  const conn = await connectDB();
+  const {idMovies} = req.params;
+  console.log(idMovies);
+  const select = "SELECT * FROM movie WHERE idMovies = ?"
+
+  const [result] = await conn.query ( select, [idMovies])
+  res.render ("detailMovie", {movie: result[0]});
+  conn.end()
+
+})
+
 const staticServerPathWeb = 'src/public-react'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathWeb));
 
 const staticServerPathImage = 'src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos
 server.use(express.static(staticServerPathImage));
+
+server.use(express.static("./src/css"));
